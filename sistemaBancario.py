3781469453804c54f5dfaf94e5ab17dd937fa5ef
João Victor Grama <149870885+Krawstr@users.noreply.github.com>
2025-03-04 19:38:25 -0300

    Update sistemaBancario.py

--- sistemaBancario.py
+++ sistemaBancario.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 
 saldo = 0
-valores_depositos = list()
-valores_saques = list()
+valores_transacoes = list()
 transacoes_diarias = 0
 LIMITE_TRANSACOES_DIARIAS = 10
 
 def saque(valor_saque):
-    global saldo, valores_saques, LIMITE_TRANSACOES_DIARIAS, transacoes_diarias
+    global valores_transacoes, LIMITE_TRANSACOES_DIARIAS, transacoes_diarias
    
     if transacoes_diarias == LIMITE_TRANSACOES_DIARIAS:
         return "Limite diario de transaçoes atingido."
@@ -15,13 +14,13 @@ def saque(valor_saque):
     if saldo >= valor_saque and valor_saque <= 500:
         saldo -= valor_saque
         transacoes_diarias += 1
-        valores_saques.append(("saque", valor_saque, datetime.now()))
-        return f"Você sacou R${valor_saque}. Seu saldo atual: R${saldo:.2f}"
+        valores_transacoes.append(("saque", valor_saque, datetime.now()))
+        return f"Você sacou R${valor_saque}."
     
     return f"Você não tem saldo suficiente para realizar o saque ou valor é maior que R$500. Seu saldo atual: R${saldo:.2f}"
 
 def deposito(valor_deposito):
-    global valores_depositos, saldo, transacoes_diarias, LIMITE_TRANSACOES_DIARIAS
+    global valores_transacoes, transacoes_diarias, LIMITE_TRANSACOES_DIARIAS
 
     if transacoes_diarias == LIMITE_TRANSACOES_DIARIAS:
         return "Limite diario de transaçoes atingido."
@@ -29,8 +28,7 @@ def deposito(valor_deposito):
     if valor_deposito <= 0:
         return f"O valor depositado deve ser maior que zero. Seu saldo atual: R${saldo:.2f}"
     saldo += valor_deposito
-    valores_saques.append(("deposito", valor_deposito, datetime.now()))
-    return f"Seu saldo atual: R${saldo:.2f}"
+    valores_transacoes.append(("deposito", valor_deposito, datetime.now()))
 
 def extrato():
     global valores_depositos, valores_saques, saldo
